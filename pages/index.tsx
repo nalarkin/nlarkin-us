4dd3856c66bc682f345ad16dc86e8233b907ced0
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-11-04 01:35:39 -0400

    added animated tile transitions to the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,52 @@
+/* eslint-disable unused-imports/no-unused-vars */
 import React from 'react';
 
-import type { NextPage } from 'next';
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 
 import Layout from '../components/layouts/layout';
+import style from './index.module.scss';
 
-const HomeContents = () => {
+const ResumeTile = () => {
   return (
-    <div className="flex justify-center items-center flex-col md:flex-row gap-7">
-      <div className="bg-white rounded-lg flex  justify-center">
+    <section className={style.tileContainer}>
+      <div className={style.cardText}>
         <Link href="/resume">
-          <a className="p-7 my-auto">Resume</a>
+          <a>
+            <h2 className={style.tileHeader}>My resume</h2>
+            <h3 className={style.subtitle}>My resume subtitle is this long</h3>
+            <div className={style.body}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
+              sequi iure molestiae modi magnam illum sapiente, doloribus ex
+              rerum ad? Soluta iure maxime praesentium mollitia ipsa autem,
+              fugiat tempora distinctio?
+            </div>
+          </a>
         </Link>
       </div>
-      <div className="bg-white rounded-lg flex  justify-center">
+      <div className={style.cardImage}></div>
+    </section>
+  );
+};
+const NewsTile = () => {
+  return (
+    <section className={style.tileContainer}>
+      <div className={style.cardText}>
         <Link href="/news">
-          <a className="p-7 my-auto">News</a>
+          <a>
+            <h2 className={style.tileHeader}>My News Project</h2>
+            <h3 className={style.subtitle}>My News project is this long</h3>
+            <div className={style.body}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
+              sequi iure molestiae modi magnam illum sapiente, doloribus ex
+              rerum ad? Soluta iure maxime praesentium mollitia ipsa autem,
+              fugiat tempora distinctio?
+            </div>
+          </a>
         </Link>
       </div>
-      {/* <Card
-        text="Would you like to see my resume?"
-        btnText="Resume"
-        href="/resume"
-      />
-      <Card text="Want to see the news?" btnText="news" href="/news" /> */}
-    </div>
+      <div className={style.cardImage}></div>
+    </section>
   );
 };
 
@@ -33,8 +55,136 @@ const HomeSEO = {
   title: 'Home',
 };
 
-const Home: NextPage = () => {
-  return <Layout seo={HomeSEO}> {HomeContents()} </Layout>;
+const container = {
+  show: {
+    opacity: 1,
+    // animation: { x: 100 },
+    transition: {
+      when: 'beforeChildren',
+      staggerChildren: 0.5,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    transition: {
+      when: 'afterChildren',
+    },
+  },
+};
+// const container = {
+//   hidden: { opacity: 0 },
+//   show: {
+//     opacity: 1,
+//     animate: { x: 100 },
+//     transition: {
+//       staggerChildren: 0.5,
+//     },
+//   },
+// };
+
+const item = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
+type StaggerProps = {
+  children: React.ReactChild[];
+  delay: number;
+  transitionTime: number;
+};
+
+const DirectionalStagger = ({
+  children,
+  delay,
+  transitionTime,
+}: StaggerProps) => {
+  // const stagger = {
+  //   show: {
+  //     opacity: 1,
+  //     // animation: { x: 100 },
+  //     // animate: { x: 500 },
+  //     transition: {
+  //       // when: 'beforeChildren',
+  //       // staggerChildren: delay * 9,
+  //     },
+  //   },
+  //   hidden: {
+  //     opacity: 0,
+  //     // transition: {
+  //     //   when: 'afterChildren',
+  //     // },
+  //   },
+  // };
+  const variants = {
+    show: (i: number) => ({
+      opacity: 1,
+      // animate: {
+      //   pathLength: 1,
+      x: 0,
+      // },
+      // x: {
+      //   0,
+      //   // type: 'spring',
+      //   // stiffness: 100,
+      //   // delay: i * delay,
+      //   // duration: transitionTime,
+      // },
+      transition: {
+        // easOut: [0.17, 0.8, 0.95, 1],
+        // type: spring,
+        delay: i * delay,
+        duration: transitionTime,
+      },
+      // duration: 1,
+    }),
+    hidden: { opacity: 0, x: 500 },
+  };
+
+  return (
+    <div className="flex justify-center items-center flex-col  gap-7">
+      <motion.ul initial="hidden" animate="show">
+        {children.map((tile, idx) => {
+          return (
+            <motion.li
+              key={idx}
+              // animate={{ x: 0 }}
+              // transition={{
+              //   type: 'spring',
+              //   duration: delay * 2,
+              //   delay: delay * idx,
+              // }}
+              variants={variants}
+              custom={idx}
+            >
+              {tile}
+            </motion.li>
+          );
+        })}
+      </motion.ul>
+    </div>
+  );
+};
+
+const HomeContents = () => {
+  return (
+    <div className="flex justify-center items-center flex-col md:flex-row gap-7 overflow-x-hidden">
+      <DirectionalStagger delay={0.7} transitionTime={1.2}>
+        <ResumeTile />
+        <NewsTile />
+        <ResumeTile />
+        <NewsTile />
+        <ResumeTile />
+      </DirectionalStagger>
+    </div>
+  );
+};
+
+const Home = () => {
+  return (
+    <Layout seo={HomeSEO}>
+      <HomeContents />
+    </Layout>
+  );
 };
 
 // const BackgroundImage = () => {
