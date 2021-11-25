fba2e55c08f667236306eed1d9707585bcc50e62
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-11-25 00:41:51 -0500

    added social bar to article pages.

--- components/article/ArticleHeaderSocial.tsx
+++ components/article/ArticleHeaderSocial.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 
-const ArticleHeaderSocial = () => {
+import { FaFacebookF, FaWhatsapp, FaTwitter } from 'react-icons/fa';
+import { IoIosMail, IoMdShareAlt } from 'react-icons/io';
+
+import style from './ArticleHeaderSocial.module.scss';
+
+interface CircleGrayButtonProps {
+  children: React.ReactElement;
+}
+const CircleGrayButton = ({ children }: CircleGrayButtonProps) => {
+  return <div className={style.circleButton}>{children}</div>;
+};
+
+const ArticleSocialBar = () => {
   return (
-    <div className="pb-6 border-b-2 border-gray-200">
-      Social Bar placeholder
+    <div className={style.socialBarContainer}>
+      <div className={style.socialBarGrid}>
+        <CircleGrayButton>
+          <FaFacebookF />
+        </CircleGrayButton>
+        <CircleGrayButton>
+          <FaWhatsapp size={20} />
+        </CircleGrayButton>
+        <CircleGrayButton>
+          <FaTwitter />
+        </CircleGrayButton>
+        <CircleGrayButton>
+          <IoIosMail size={20} />
+        </CircleGrayButton>
+        <CircleGrayButton>
+          <IoMdShareAlt size={20} />
+        </CircleGrayButton>
+      </div>
     </div>
   );
 };
 
-export default ArticleHeaderSocial;
+export default ArticleSocialBar;
