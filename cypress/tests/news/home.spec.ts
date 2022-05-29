4ffe0aa51bb398cc9a2821d9289a0af36e1ffa4c
Nathan Larkin <nathanlarkin.usa@gmail.com>
2022-05-28 22:14:56 -0400

    moved cypress test folder

---
+++ cypress/tests/news/home.spec.ts
@@ -0,0 +1,33 @@
+// src/setupTests.js
+
+// import { server } from '../../../mocks/server';
+
+// Establish API mocking before all tests.
+
+// before(() => server.listen());
+
+// Reset any request handlers that we may add during the tests,
+
+// so they don't affect other tests.
+
+// Clean up after the tests are finished.
+
+// after(() => server.close());
+
+// describe('News home page', () => {
+//   beforeEach(() => {
+//     cy.visit('/news');
+//
+//     // cy.intercept('/**').as('catch-all');
+//   });
+//
+//   // afterEach(() => server.resetHandlers());
+//
+//   it('intercepts props call', () => {
+//     cy.wait('@catch-all').then((res) => {
+//       // cy.log(res.request);
+//       // cy.log(res.response);
+//     });
+//     //
+//   });
+// });
